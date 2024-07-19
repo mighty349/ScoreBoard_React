8338adbfd866f0c783a6eefc7c943e9b366fa3f5
Rama Krishna L <mightywarrior349@gmail.com>
2024-07-19 19:25:59 +0530

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/ScoreBoard_React/' // Replace 'repository-name' with the name of your GitHub repository
-});
